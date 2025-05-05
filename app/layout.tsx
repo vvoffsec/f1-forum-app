bdd8ee0ba979a690e6632440c87499173b0b3d60
Vishal Venkateswaran <113947350+vvoffsec@users.noreply.github.com>
2025-05-05 12:01:12 -0700

    Added Clerk API to env and Vercel ENV

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import "styles/globals.css";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
       appearance={{
         baseTheme: dark,
         variables: {
